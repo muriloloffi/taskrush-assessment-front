e5eda951c47e491aac565812191608aa54e18739
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-16 09:43:42 -0300

    Reconfig react-router; Install Tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Projects from "./components/Projects";
+import Project from "./components/Project";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <Projects /> },
+  { path: "/project/:id", component: Project },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
