f14bc5fe4783d25c92691598891621bf52e4d7e5
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-16 09:53:47 -0300

    Correctly display projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,24 @@
-import { useFetch } from '../useFetch';
-import { Link } from 'react-router-dom';
+import { useFetch } from "../useFetch";
+import { Link } from "react-router-dom";
 
 function Projects() {
-    const { data: projects, loading, error } = useFetch('/api/projects');
+  const { data: projects, loading, error } = useFetch("/api/projects");
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
-    return (
-        <div>
-            {projects.map(project => (
-                <div key={project.id}>
-                    <Link to={`/project/${project.id}`}>{project.name}</Link>
-                </div>
-            ))}
+  return (
+    <div className="flex flex-col items-start">
+      {projects.map((project) => (
+        <div
+          key={project.id}
+          className="p-4 m-2 bg-gray-100 rounded-md shadow-md"
+        >
+          <Link to={`/project/${project.id}`}>{project.title}</Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
