416492eee838c34b82fbd90d1711314ffe52714d
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-18 15:08:10 -0300

    Project export according to convention

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useFetch } from "../helpers/useFetch";
 import Task from "./task";
 
-export function Project() {
+function Project() {
   const { id } = useParams();
   const { data: project, loading } = useFetch(`/api/project/${id}`);
 
