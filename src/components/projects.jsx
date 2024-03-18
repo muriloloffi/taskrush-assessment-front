8ecb6c09be1ebb1c5ed5ac5c142178750be5d8f1
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-18 12:19:20 -0300

    Improve projects navigation bar

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -9,7 +9,8 @@ function Projects() {
 
   return (
     <>
-      <div className="flex flex-col items-start w-80 border-r border-solid border-slate-200">
+      <div className="flex flex-col items-center w-80 border-r border-solid border-slate-200">
+        <h1 className="text-2xl font-bold text-center w-full">Projetos</h1>
         {projects.map((project) => (
           <Link to={`/project/${project.id}`} key={project.id}>
             <div className="p-4 m-2 bg-gray-300 rounded-md shadow-md text-blue-500 hover:shadow-slate-300">
